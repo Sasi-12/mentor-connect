a09834a5fe56224ec8def729649e7fbfd36c9f8f
Sasi367 <sasikumari0604@gmail.com>
2025-01-22 14:18:59 +0530

    commits

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client'; // React 18+
-import { BrowserRouter as Router } from 'react-router-dom'; // <--- Router here
+import { BrowserRouter as Router } from 'react-router-dom'; // Router included
 import App from './App';
 import './index.css';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    <Router>
+        <App />
+    </Router>
 );
